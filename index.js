8a28736b05a4edf79c17d421cc573f7adf9fccec
yoshuawuyts <i@yoshuawuyts.com>
2015-01-12 13:16:18 +0100

    deps: bump dto version

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ const fs      = require('fs')
 module.exports = createIndex
 
 // Create directory index file.
-// @param {String} src
+// @param {String} opts
 // @param {String} dest
 // @param {Function} cb
-function createIndex(src, dest, cb) {
-  assert.equal(typeof src, 'string')
+function createIndex(opts, dest, cb) {
+
+  opts = 'string' == typeof opts
+    ? {path: opts}
+    : opts
+
+  assert.equal(typeof opts, 'object')
   assert.equal(typeof dest, 'string')
   assert.equal(typeof cb, 'function')
 
-  debug(src, dest)
+  debug(opts, dest)
 
-  readDir(src, function(err, res) {
+  readDir(opts, function(err, res) {
     debug(err, res)
     if (err) return cb(err)
     writeFile(dest, res, cb)
@@ -26,10 +31,13 @@ function createIndex(src, dest, cb) {
 }
 
 // Read file from `src`.
-// @param {String} dest
+// @param {String} opts
 // @param {function} cb
-function readDir(src, cb) {
-  dto(path.resolve(src), cb)
+function readDir(opts, cb) {
+  assert.equal(typeof opts, 'object')
+  assert.equal(typeof opts.path, 'string')
+  assert.equal(typeof cb, 'function')
+  dto(opts, cb)
 }
 
 // Write file to `dest`.
